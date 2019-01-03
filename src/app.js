04f9c9683769271e1d5f7e416ec0a5dc9b798b2e
Bartosz Mediger <bartosz@mediger.net>
2019-01-03 18:10:13 +0100

    RWD, navigation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import css from './sass/style.scss';
-import $ from 'jquery';
 
 require('./js/common_new.js');
 require('./js/nav.js');
--- src/js/nav.js
+++ src/js/nav.js
@@ -1,12 +1,101 @@
+const bodyScrollLock = require('body-scroll-lock');
+const disableBodyScroll = bodyScrollLock.disableBodyScroll;
+const enableBodyScroll = bodyScrollLock.enableBodyScroll;
 
 document.addEventListener('DOMContentLoaded',function() {
     
     const header = document.getElementById('header');
+    const navigation = document.getElementsByClassName('navigation')[0];
+    const submenuParent = navigation.getElementsByTagName('span');
+    let hamburger;
+
+    // If navigation is higher than window
+    const setHeight = function() {
+
+        let windowHeight = window.innerHeight,
+            navigationHeight = navigation.getElementsByClassName('current')[0].clientHeight;
+        
+        if (navigationHeight > windowHeight) {
+            navigation.classList.add('to-baseline');
+        } else {
+            navigation.classList.remove('to-baseline');
+        }        
+    };
     
-    let tmp = document.createElement('div');
+    const closeSubmenus = function() {
+        let openedSubmenus = document.getElementsByClassName('submenu-visible');
+                
+        for (let i = 0; i < openedSubmenus.length; i ++) {
+            openedSubmenus[i].classList.remove('submenu-visible');
+        }
+    }
 
-    tmp.innerHTML = '<div class="o-hamburger js-hamburger"></div>';
+    // Open submenu
+    const submenu = function() {
+    
+        const action = function(e) {
+        
+            let currentSubmenu = e.currentTarget.nextElementSibling;
+            
+            if (currentSubmenu.classList.contains('submenu-visible')) {
+                currentSubmenu.classList.remove('submenu-visible');
+            } else {
+                closeSubmenus();
+                currentSubmenu.classList.add('submenu-visible');
+            }
 
-    header.appendChild(tmp.firstChild);    
+            setHeight();                
+        };
+
+        for (let i = 0; i < submenuParent.length; i ++) {
+            submenuParent[i].addEventListener('click', action);
+        }
+    }
+    
+    // Close Nav
+    const closeNav = function() {
+        enableBodyScroll(navigation);
+        navigation.classList.remove('is-visible');
+        hamburger.classList.remove('is-active');
+        window.removeEventListener('resize', setHeight);
+        navigation.removeEventListener("transitionend", setHeight, false);
+        closeSubmenus();
+    };
+    
+    // Open Nav
+    const openNav = function() {
+        disableBodyScroll(navigation);
+        navigation.classList.add('is-visible');
+        hamburger.classList.add('is-active');
+        window.addEventListener('resize', setHeight);
+        navigation.addEventListener("transitionend", setHeight, false);
+        
+    };
+    
+    // Init nav
+    const nav = function(e) {
+    
+        if (hamburger.classList.contains('is-active')) {
+            closeNav();
+        } else {
+            openNav();
+        }
+    };
+    
+    // Add hamburger to page
+    const addHamburger = function() {        
+    
+        let tmp = document.createElement('div');
+
+        tmp.innerHTML = '<div class="o-hamburger js-hamburger"></div>';
+        header.appendChild(tmp.firstChild);
+        
+        hamburger = document.getElementsByClassName('js-hamburger')[0];
+        hamburger.addEventListener('click', nav);
+    };
+    
+    
+    addHamburger();
+    submenu();
     
 }, false);
